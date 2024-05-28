cf9fd4fded9d8ab21997a60ca0e2b40ffa9175a7
mvdebolskiy <80036033+mvdebolskiy@users.noreply.github.com>
2024-05-28 16:14:02 +0200

    add hello script

--- src/hello.sh
+++ src/hello.sh
@@ -1,3 +1,87 @@
 #!/bin/bash
 
+# script to be called by wrap_srun+preproc.sh
 
+SCRIPT=$( basename  "$0" )
+
+
+# functions for output
+function help
+{
+  echo "$SCRIPT for saying hello to someone"
+  echo ""
+  usage
+}
+
+
+function usage
+{
+
+    local txt=(
+"Usage: $SCRIPT [options] [arguments]"
+""
+"Options:"
+"  --help, -h          Print help."
+"  --who, -w           Who to say hello to"
+
+""
+"  defaults are:"
+"  who = World"
+    )
+
+    printf "%s\n" "${txt[@]}"
+}
+
+function badusage
+{
+  local message="$1"
+  local txt=(
+  "For overview  execute:"
+  "$SCRIPT --help"
+  )
+  [[ $message ]] && printf "$message\n"
+  printf "%s\\n" "${txt[@]}"
+}
+
+#define vars
+who=""
+
+
+#process arguments
+PARSEDARGS=$(getopt -o h:w --long help,who -- "$@")
+
+if [ "$?" != "0" ]; then
+  badusage
+fi
+
+eval set -- "$PARSEDARGS"
+
+while true; do
+    case $1 in
+      --help | -h)
+        help
+        exit;;
+      --GCM | -g)
+        who=$2 ; shift 2 ;;
+      --) shift ; break ;;
+      *) badusage "$1" ; shift ; exit ;;
+    esac
+done
+
+
+
+# errors when vars are not set by args
+
+if [[ -z "$who" ]] ; then
+    echo
+    echo "Who is not defined"
+    echo ""
+    echo ""
+    echo "Setting to default:"
+    who="World"
+    echo "Hello ${who}!!"
+    exit
+fi
+
+
+echo "Hello ${who}!!"
